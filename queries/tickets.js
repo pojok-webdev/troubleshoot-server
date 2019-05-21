79c02401be3e675c6961439429d9c41b6f349d0a
pujie <pw.prayitno@gmail.com>
2019-05-21 15:48:24 +0700

    print query

--- queries/tickets.js
+++ queries/tickets.js
@@ -1,10 +1,12 @@
 gets = () => {
     sql = "select * from tickets "
+    console.log("SQL ticket gets",sql)
     return sql
 }
 get = obj => {
     sql = "select * from tickets "
     sql+= "where id = " + obj.id + " "
+    console.log("SQL ticket get",sql)
     return sql
 }
 module.exports = {
--- queries/troubleshoots.js
+++ queries/troubleshoots.js
@@ -1,12 +1,14 @@
 gets = () => {
     sql = "select a.id,b.clientname name,b.kdticket from troubleshoot_requests a "
     sql+= "left outer join tickets b on b.id=a.ticket_id "
+    console.log("SQL troubleshoot gets",sql)
     return sql
 }
 get = obj => {
     sql = "select a.id,b.clientname name,b.kdticket from troubleshoot_requests a "
     sql+= "left outer join tickets b on b.id=a.ticket_id "
     sql+= "where a.id = " + obj.id +" "
+    console.log("SQL troubleshoot get",sql)
     return sql
 }
 module.exports = {
