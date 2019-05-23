bdcc1bdd98d64ae15cc5a7042c641774fb0a472f
pujie <pw.prayitno@gmail.com>
2019-05-23 07:31:52 +0700

    fix Access-Control-Allow-Headers

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require("express"),
     troubleshoots = require("./queries/troubleshoots");
 app.use((req,res,next)=>{
     res.header("Access-Control-Allow-Origin","*")
-    res.header("Access-Control-Alow-Headers","Origin,X-Requested-With,Content-Type,Accept")
+    res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept")
     next()
 })
 app.use(bodyParser.json({limit:'10mb',extended:true}))
