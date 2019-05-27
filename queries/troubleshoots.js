6abc79c0b53614f5cb840935dc66befa5d7df65f
pujie <pw.prayitno@gmail.com>
2019-05-27 11:07:52 +0700

    checklistcount int troubleshoot query

--- queries/troubleshoots.js
+++ queries/troubleshoots.js
@@ -8,24 +8,30 @@ gets = () => {
     return sql
 }
 get = obj => {
-    sql = "select a.id,b.clientname name,b.kdticket from troubleshoot_requests a "
+    sql = "select a.id,b.clientname name,b.kdticket,count(c.id)checklistcount from troubleshoot_requests a "
     sql+= "left outer join tickets b on b.id=a.ticket_id "
+    sql+= "left outer join troubleshootchecklists c on c.troubleshoot_id=a.id "
+    sql+= "group by a.id,b.clientname,b.kdticket "
     sql+= "where a.id = " + obj.id +" "
     sql+= "order by b.kdticket desc "
     console.log("SQL troubleshoot get",sql)
     return sql
 }
 getslimit = obj => {
-    sql = "select a.id,b.clientname name,b.kdticket from troubleshoot_requests a "
+    sql = "select a.id,b.clientname name,b.kdticket,count(c.id)checklistcount from troubleshoot_requests a "
     sql+= "left outer join tickets b on b.id=a.ticket_id "
+    sql+= "left outer join troubleshootchecklists c on c.troubleshoot_id=a.id "
+    sql+= "group by a.id,b.clientname,b.kdticket "
     sql+= "order by b.kdticket desc "
     sql+= "limit " + obj.segment + ", " + obj.offset + " "
     console.log("SQL troubleshoot gets",sql)
     return sql
 }
 search = obj => {
-    sql = "select a.id,b.clientname name,b.kdticket from troubleshoot_requests a "
+    sql = "select a.id,b.clientname name,b.kdticket,count(c.id)checklistcount from troubleshoot_requests a "
     sql+= "left outer join tickets b on b.id=a.ticket_id "
+    sql+= "left outer join troubleshootchecklists c on c.troubleshoot_id=a.id "
+    sql+= "group by a.id,b.clientname,b.kdticket "
     sql+= "where b.clientname like '%" + obj.clientname + "%' or b.kdticket like '%" + obj.kdticket + "%' "
     sql+= "order by b.kdticket desc "
     sql+= "limit " + obj.segment + ", " + obj.offset + " "
