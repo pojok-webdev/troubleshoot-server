1504ac408affc880055b620b12d840f47197469e
pujie <pw.prayitno@gmail.com>
2019-05-29 08:17:35 +0700

    show client and ticket on ticket table

--- queries/troubleshootchecklists.js
+++ queries/troubleshootchecklists.js
@@ -1,6 +1,12 @@
 getByTroubleshoot = obj => {
     sql = "select * from troubleshootchecklists "
     sql+= "where troubleshoot_id = " + obj.troubleshoot_id + " "
+    sql = "select a.id,c.kdticket,e.name,a.problemtype from troubleshootchecklists a "
+    sql+= "left outer join troubleshoot_requests b on b.id=a.troubleshoot_id "
+    sql+= "left outer join tickets c on c.id=b.ticket_id "
+    sql+= "left outer join client_sites d on d.id=b.client_site_id "
+    sql+= "left outer join clients e on e.id=d.client_id "
+    sql+= "where a.troubleshoot_id = " + obj.troubleshoot_id + " "
     return sql; 
 }
 saveChecklist = obj => {
