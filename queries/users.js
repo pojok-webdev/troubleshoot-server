085b8e14829437b04ec71c07d6299ef93f47c1e5
pujie <pw.prayitno@gmail.com>
2019-05-29 14:41:52 +0700

    add argument to user query

--- queries/users.js
+++ queries/users.js
@@ -2,7 +2,7 @@ gets = obj => {
     sql = "select a.id,a.email,username,jobdesc,a.group_id,c.name from users a "
     sql+= "left outer join groups_users b on b.user_id=a.id "
     sql+= "left outer join groups c on c.id=b.group_id "
-    sql+= "where c.id = " + obj.group_id + " "
+    sql+= "where c.id = " + obj.group_id + " and a.active='1' "
     console.log("SQL",sql)
     return sql
 }
